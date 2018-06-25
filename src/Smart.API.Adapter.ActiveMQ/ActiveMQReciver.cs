def8fa63c4f6ea3f1146cf088afce9003e7f4dce
liangfengli <915098904@qq.com>
2018-06-25 13:52:12 +0800

    添加ActiveMQ

---
+++ src/Smart.API.Adapter.ActiveMQ/ActiveMQReciver.cs
@@ -0,0 +1,102 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Smart.API.Adapter.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart.API.Adapter.ActiveMQ
+{
+    public class ActiveMQReciver
+    {
+        /// <summary>
+        /// 订阅消息委托
+        /// </summary>
+        /// <param name="message"></param>
+        public delegate void ActiveMQReciveMsg(string message);
+
+        /// <summary>
+        /// 订阅消息事件
+        /// </summary>
+        public event ActiveMQReciveMsg OnActiveMQReciveMsg;
+
+
+        private static IConnectionFactory _IConnectionFactory;
+        public ActiveMQReciver()
+        {
+            try
+            {
+                if (_IConnectionFactory == null)
+                {
+                    _IConnectionFactory = new ConnectionFactory(CommonSettings.ActiveMQUrl);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error("初始化ActiveMQ服务端错误,", ex);
+            }
+        }
+
+        public void StartReciveMsg(string queueName = null, string filter = null)
+        {
+            //开始连接.用户名密码              
+            IConnection connection = CreateConnection();
+            //如果你要持久“订阅”，则需要设置ClientId，这样程序运行当中被停止，恢复运行时，能拿到没接收到的消息！   
+            connection.ClientId = "SmartAPIAdapter";
+            // 启动连接  
+            connection.Start();
+            // 创建一个session会话  
+            ISession session = connection.CreateSession();
+            if (string.IsNullOrWhiteSpace(queueName))
+            {
+                queueName = CommonSettings.ActiveMQQueue;
+            }
+            //Create the Consumer  
+            IMessageConsumer consumer;
+            if (string.IsNullOrWhiteSpace(filter))
+            {
+                consumer = session.CreateConsumer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue(queueName));
+            }
+            else
+            {
+                consumer = session.CreateConsumer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue(queueName), "filter = '" + filter + "'");
+            }
+            //接收数据            
+            consumer.Listener += new MessageListener(consumer_Listener);
+        }
+
+        /// <summary>
+        /// 接收消息的订阅事件
+        /// </summary>
+        /// <param name="message"></param>
+        private void consumer_Listener(IMessage message)
+        {
+            ITextMessage msg = (ITextMessage)message;
+            if (OnActiveMQReciveMsg != null)
+            {
+                OnActiveMQReciveMsg(msg.Text);
+            }
+        }
+
+
+        /// <summary>
+        /// 创建连接
+        /// </summary>
+        /// <returns></returns>
+        private static IConnection CreateConnection()
+        {
+            IConnection connection;
+            if (!string.IsNullOrEmpty(CommonSettings.ActiveMQName))
+            {
+                connection = _IConnectionFactory.CreateConnection(CommonSettings.ActiveMQName, CommonSettings.ActiveMQPassword);
+            }
+            else
+            {
+                connection = _IConnectionFactory.CreateConnection();
+            }
+            return connection;
+        }
+    }
+}
---
+++ src/Smart.API.Adapter.ActiveMQ/ActiveMQSender.cs
@@ -0,0 +1,90 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Smart.API.Adapter.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart.API.Adapter.ActiveMQ
+{
+    public class ActiveMQSender
+    {
+        private static IConnectionFactory _IConnectionFactory;
+
+        /// <summary>
+        /// 初始化ActiveMQ服务
+        /// </summary>       
+        static ActiveMQSender()
+        {
+            try
+            {
+                if (_IConnectionFactory == null)
+                {
+                    _IConnectionFactory = new ConnectionFactory(CommonSettings.ActiveMQUrl);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error("初始化ActiveMQ服务端错误,", ex);
+            }
+        }
+
+        /// <summary>
+        /// 发送消息到队列
+        /// </summary>
+        /// <param name="tcpUrl">ActiveMQ服务地址</param>
+        /// <param name="message">发送的消息内容</param>
+        /// <param name="queueName">队列名</param>
+        /// <param name="filter">过滤关键字</param>
+        public static void SendMQMessage(string message, string queueName = null, string filter = null)
+        {
+            //建立连接
+            using (IConnection connection = CreateConnection())
+            {
+                //创建Session会话
+                using (ISession session = connection.CreateSession())
+                {
+                    if (string.IsNullOrWhiteSpace(queueName))
+                    {
+                        queueName = CommonSettings.ActiveMQQueue;
+                    }
+                    //创建MQ的Queue
+                    IMessageProducer prod = session.CreateProducer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue(queueName));
+                    //创建一个发送消息的对象
+                    ITextMessage IMessage = prod.CreateTextMessage();
+                    IMessage.Text = message; //给这个消息对象赋实际的消息
+                    //设置消息对象的过滤关键字属性
+                    if (!string.IsNullOrWhiteSpace(filter))
+                    {
+                        IMessage.Properties.SetString("filter", filter);
+                    }
+                    //NonPersistent 非持久化消息
+                    prod.DeliveryMode = MsgDeliveryMode.NonPersistent;
+                    prod.Priority = MsgPriority.Normal;
+                    prod.TimeToLive = TimeSpan.MinValue;
+                    prod.Send(IMessage);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 创建连接
+        /// </summary>
+        /// <returns></returns>
+        private static IConnection CreateConnection()
+        {
+            IConnection connection;
+            if (!string.IsNullOrEmpty(CommonSettings.ActiveMQName))
+            {
+                connection = _IConnectionFactory.CreateConnection(CommonSettings.ActiveMQName, CommonSettings.ActiveMQPassword);
+            }
+            else
+            {
+                connection = _IConnectionFactory.CreateConnection();
+            }
+            return connection;
+        }
+    }
+}
