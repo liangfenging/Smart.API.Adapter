7ce15e46dfc79dc55e89ccf58351a357273606a4
915098904@qq.com <915098904@qq.com>
2018-08-22 14:17:02 +0800

    添加导出日志

---
+++ src/Smart.API.Adapter.BizCore/JD/VehicleLogSqlBLL.cs
@@ -0,0 +1,40 @@
+﻿using Smart.API.Adapter.DataAccess.Core.JD;
+using Smart.API.Adapter.Models.DTO.JD;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart.API.Adapter.BizCore.JD
+{
+    public class VehicleLogSqlBLL
+    {
+
+        public void InsertVehicleLogSql(VehicleLogSql info)
+        {
+            new VehicleLogSqlDAL().Insert<VehicleLogSql>(info);
+        }
+
+        public DataTable GetDTVehicleLog(DateTime dtStart, DateTime dtEnd, int limitStart, int limitEnd)
+        {
+            return new VehicleLogSqlDAL().GetDTVehicleLog(dtStart, dtEnd, limitStart, limitEnd);
+
+        }
+        public int GetCountVehicleLog(DateTime dtStart, DateTime dtEnd)
+        {
+            return new VehicleLogSqlDAL().GetCountVehicleLog(dtStart, dtEnd);
+        }
+
+        public int GetInCountVehicleLog(DateTime dtStart, DateTime dtEnd)
+        {
+            return new VehicleLogSqlDAL().GetInCountVehicleLog(dtStart, dtEnd);
+        }
+
+        public int GetOutCountVehicleLog(DateTime dtStart, DateTime dtEnd)
+        {
+            return new VehicleLogSqlDAL().GetOutCountVehicleLog(dtStart, dtEnd);
+        }
+    }
+}
---
+++ src/Smart.API.Adapter.DataAccess/Core/JD/VehicleLogSqlDAL.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart.API.Adapter.DataAccess.Core.JD
+{
+    public class VehicleLogSqlDAL : DataBase
+    {
+        public VehicleLogSqlDAL()
+            : base(DbName.SmartAPIAdapterCore, "vehiclelogsql", "ID")
+        { }
+
+
+        public DataTable GetDTVehicleLog(DateTime dtStart, DateTime dtEnd, int limitStart, int limitEnd)
+        {
+            string sql = @"SELECT ID as 序号,postTime as 推送时间,if(result=0,'N','Y') as 推送结果, actionDescId, vehicleNo, parkLotCode,actionPositionCode,actionPosition,actionTime ,
+                                LogNo, entryTime,reasonCode,reason,photoStr,photoName,resend
+                                FROM vehiclelogsql  where DATE(postTime)<=@dtEnd and DATE(postTime)>=@dtStart limit " + limitStart + "," + limitEnd;
+
+
+
+
+            using (DbCommand cmd = db.GetSqlStringCommand(sql))
+            {
+
+                db.AddInParameter(cmd, "@dtStart", DbType.Date, dtStart);
+                db.AddInParameter(cmd, "@dtEnd", DbType.Date, dtEnd);
+
+                DataTable dt = db.ExecuteDataSet(cmd).Tables[0];
+
+                return dt;
+            }
+        }
+
+        public int GetCountVehicleLog(DateTime dtStart, DateTime dtEnd)
+        {
+            int count = 0;
+
+            string sql = @"SELECT count(0)   FROM vehiclelogsql   where DATE(postTime)<=@dtEnd and DATE(postTime)>=@dtStart";
+
+            using (DbCommand cmd = db.GetSqlStringCommand(sql))
+            {
+
+                db.AddInParameter(cmd, "@dtStart", DbType.Date, dtStart);
+                db.AddInParameter(cmd, "@dtEnd", DbType.Date, dtEnd);
+
+                DataTable dt = db.ExecuteDataSet(cmd).Tables[0];
+                if (dt != null && dt.Rows.Count > 0)
+                    count = Convert.ToInt32(dt.Rows[0][0].ToString());
+            }
+
+            return count;
+        }
+
+        public int GetInCountVehicleLog(DateTime dtStart, DateTime dtEnd)
+        {
+            int count = 0;
+
+            string sql = @"SELECT count(0)   FROM vehiclelogsql   where DATE(postTime)<=@dtEnd and DATE(postTime)>=@dtStart and actionDescId in (1,2) and resend = 1";
+
+            using (DbCommand cmd = db.GetSqlStringCommand(sql))
+            {
+
+                db.AddInParameter(cmd, "@dtStart", DbType.Date, dtStart);
+                db.AddInParameter(cmd, "@dtEnd", DbType.Date, dtEnd);
+
+                DataTable dt = db.ExecuteDataSet(cmd).Tables[0];
+                if (dt != null && dt.Rows.Count > 0)
+                    count = Convert.ToInt32(dt.Rows[0][0].ToString());
+            }
+
+            return count;
+        }
+
+
+        public int GetOutCountVehicleLog(DateTime dtStart, DateTime dtEnd)
+        {
+            int count = 0;
+
+            string sql = @"SELECT count(0)   FROM vehiclelogsql   where DATE(postTime)<=@dtEnd and DATE(postTime)>=@dtStart and actionDescId in (4,5) and resend = 1";
+
+            using (DbCommand cmd = db.GetSqlStringCommand(sql))
+            {
+
+                db.AddInParameter(cmd, "@dtStart", DbType.Date, dtStart);
+                db.AddInParameter(cmd, "@dtEnd", DbType.Date, dtEnd);
+
+                DataTable dt = db.ExecuteDataSet(cmd).Tables[0];
+                if (dt != null && dt.Rows.Count > 0)
+                    count = Convert.ToInt32(dt.Rows[0][0].ToString());
+            }
+
+            return count;
+        }
+    }
+}
